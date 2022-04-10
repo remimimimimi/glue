c791a169e3f6bf2d1fde1a7157ab0bade8bf1243
remimimimimi <valent.xarin@gmail.com>
2022-04-11 00:11:21 +0300

    Add first bozon nix package

--- flake.nix
+++ flake.nix
@@ -1,7 +1,22 @@
 {
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs"; };
-
-  output = { self, nixpkgs }: {
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        bozon = pkgs.rustPlatform.buildRustPackage rec {
+          pname = "bozon";
+          version = "0.1.0";
+          src = self;
+          cargoSha256 = "kasdf";
+        };
+      in {
+        defaultPackage = bozon;
+        packages = bozon;
+        devShell = pkgs.mkShell { buildInputs = with pkgs; [ hello cowsay ]; };
+      });
 }
