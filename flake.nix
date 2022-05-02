005596d18b7da914485fde17cb6d51f12d4dac8d
remimimimimi <valent.xarin@gmail.com>
2022-05-02 19:33:37 +0300

    Change rust overlay in flake.nix

--- flake.nix
+++ flake.nix
@@ -3,27 +3,28 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    naersk.url = "github:nmattia/naersk";
-    naersk.inputs.nixpkgs.follows = "nixpkgs";
-    utils.url = "github:numtide/flake-utils";
-    mozillapkgs = {
-      url = "github:mozilla/nixpkgs-mozilla";
-      flake = false;
+    naersk = {
+      url = "github:nmattia/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils, naersk, mozillapkgs }:
-    utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, naersk, fenix, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages."${system}";
-        mozilla = pkgs.callPackage (mozillapkgs + "/package-set.nix") { };
-        rust = (mozilla.rustChannelOf {
-          rustToolchain = ./rust-toolchain;
-          sha256 = "AtA8M47G6aCq/VqFywceRWX28BLhKEuXrg7YAZ3PUHs=";
-        }).rust;
-        naersk-lib = naersk.lib."${system}".override {
-          cargo = rust;
-          rustc = rust;
+        pkgs = nixpkgs.legacyPackages.${system};
+        rust-toolchain = fenix.packages.${system}.fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "otgm+7nEl94JG/B+TYhWseZsHV1voGcBsW/lOD2/68g=";
+        };
+        naersk-lib = naersk.lib.${system}.override {
+          cargo = rust-toolchain;
+          rustc = rust-toolchain;
         };
       in rec {
         # `nix build`
@@ -38,12 +39,12 @@
         defaultPackage = packages.bozon;
 
         # `nix run`
-        apps.bozon = utils.lib.mkApp { drv = packages.bozon; };
+        apps.bozon = flake-utils.lib.mkApp { drv = packages.bozon; };
         defaultApp = apps.bozon;
 
         # `nix develop`
         devShell = pkgs.mkShell {
-          nativeBuildInputs = [ rust pkgs.llvmPackages_13.libllvm ];
+          nativeBuildInputs = [ rust-toolchain pkgs.llvmPackages_13.libllvm ];
           LIBCLANG_PATH = "${pkgs.llvmPackages_13.libclang.lib}/lib";
         };
       });
