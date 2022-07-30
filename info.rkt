3a55fcd19e8513baaa7d39dc62c7c739737d8399
remimimimimi <remimimimimi@protonmail.com>
2022-07-30 05:43:00 +0300

    One more fix

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 (define collection "glue")
 (define deps '("base" "nanopass"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/Glue.scrbl" ())))
+(define scribblings '(("scribblings/glue.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.1.0")
 (define pkg-authors '(remi))
