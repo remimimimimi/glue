ef2ecdb75b5e0d4b81537550ee13443f1f14337b
remimimimimi <remimimimimi@protonmail.com>
2022-07-30 05:41:37 +0300

    Fix typo

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "glue")
 (define deps '("base" "nanopass"))
-(define build-deps '("scribble-lib" () "racket-doc" "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/Glue.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.1.0")
