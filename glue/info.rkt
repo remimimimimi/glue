8566d30c3e60238c03a09053ab98c21a3eaa9702
remimimimimi <remimimimimi@protonmail.com>
2022-08-22 05:54:02 +0300

    Change nanopass to turnstile

--- glue/info.rkt
+++ glue/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "glue")
-(define deps '("base" "nanopass"))
+(define deps '("base" "turnstile"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/glue.scrbl" ())))
 (define pkg-desc "Description Here")
