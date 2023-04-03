bca6983150f6b3e74908a7d91ef8cab09426627d
remimimimimi <remimimimimi@protonmail.com>
2023-04-03 11:29:18 +0300

    Implement basic environment class

--- src/main.lisp
+++ src/main.lisp
@@ -2,4 +2,23 @@
   (:use :cl))
 (in-package :glue)
 
-;; Define basic type of a file content
+(defclass environment ()
+  ((scopes
+    :initform (list (make-hash-table))
+    :accessor scopes)))
+
+(defun make-environment ()
+  (make-instance 'environment))
+
+(defmethod scope-push ((env environment))
+  (setf (scopes env) (cons (make-hash-table) (scopes env))))
+
+(defmethod binding-add ((env environment) (s symbol) value)
+  (setf (gethash s (car (scopes env))) value))
+
+(defmethod binding-get ((env environment) (s symbol))
+  (some #'(lambda (scope) (gethash s scope)) (scopes env)))
+
+(defmethod scope-pop ((env environment))
+  ;; Maybe add additional check for nil?
+  (setf (scopes env) (cdr (scopes env))))
